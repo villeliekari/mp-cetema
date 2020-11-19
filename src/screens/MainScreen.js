317f8f04df5342747bd7a36b02ad038465520962
Jere Laitila <jere.laitila@metropolia.fi>
2020-11-19 20:26:09 +0200

    first alert push

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "native-base";
+import { Container, Fab, Button, View, Header, Icon } from "native-base";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Alert } from 'react-native';
 import { mapStyleDark } from "../styles/MapStyleDark";
 import * as Location from "expo-location";
+import * as geofirestore from 'geofirestore';
+import * as geokit from 'geokit';
 
-import fb from "../helpers/Firebase";
+import firebase from "../helpers/Firebase";
 
 const MainScreen = () => {
   const [location, setLocation] = useState(null);
@@ -13,6 +16,8 @@ const MainScreen = () => {
   const [userLocations, setUserLocations] = useState(null);
   const [shipMarkers, setShipMarkers] = useState([]);
   const [userMarkers, setUserMarkers] = useState([]);
+  const [userSosData, setUserSosData] = useState([]);
+  const [active, setActive] = useState(false)
 
   const fetchData = async () => {
     console.log("Fetching data...");
@@ -24,17 +29,13 @@ const MainScreen = () => {
       "https://meri.digitraffic.fi/api/v1/metadata/vessels"
     ).then(success);
 
-    fb.database()
-      .ref("/userLocations")
-      .orderByKey()
-      .once("value")
-      .then((snap) => {
-        let userLocationsData = [];
-        snap.forEach((childSnap) => {
-          userLocationsData.push(childSnap.val());
-        });
-        setUserLocations(userLocationsData);
+    firebase.firestore().collection('userLocations').get().then(snap => {
+      let userLocationsData = [];
+      snap.forEach((childSnap) => {
+        userLocationsData.push(childSnap.data());
       });
+      setUserLocations(userLocationsData);
+    });
 
     try {
       const [locationsFetch, metadataFetch] = await Promise.all([
@@ -65,19 +66,25 @@ const MainScreen = () => {
     }
   };
 
-  const updateUserLocationToFirebase = () => {
+  const updateUserLocation = () => {
     if (location) {
       const currentLocation = {
         ...location,
-        uid: fb.auth().currentUser.uid,
-        username: fb.auth().currentUser.displayName,
+        uid: firebase.auth().currentUser.uid,
+        username: firebase.auth().currentUser.displayName,
         //boatname and so on.
       };
-      fb.database()
-        .ref(`/userLocations/${fb.auth().currentUser.uid}`)
-        .set(currentLocation);
+      firebase.firestore()
+        .collection('userLocations')
+        .doc(firebase.auth().currentUser.uid)
+        .set(currentLocation, { merge: true })
+        .then((doc) => {
+          console.log('New Location document added');
+        }).catch((error) => {
+          console.error('Error adding document: ', error);
+        });
     }
-  };
+  }
 
   const getUserLocation = async () => {
     console.log("Getting user location...");
@@ -90,76 +97,189 @@ const MainScreen = () => {
     setLocation(_location);
   };
 
+  const sosAlert = () => {
+    Alert.alert(
+      'SOS Alert',
+      'People nearby will receive your alert',
+      [
+        { text: 'Rescued', onPress: () => updateSosAlert('rescued') },
+        { text: 'Cancel', onPress: () => updateSosAlert('cancel'), style: 'cancel' }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const receiveSosAlert = () => {
+    if (userSosData) {
+      console.log("xxxxxxxx", userSosData)
+      Alert.alert(
+        'SOS Alert',
+        userSosData.username + ' needs your help',
+        [
+          { text: 'Ok' },
+          { text: 'Cancel', style: 'cancel' }
+        ],
+        { cancelable: true }
+      )
+    }
+  }
+
+  const updateSosAlert = (option) => {
+    if (option == 'rescued') {
+      firebase.firestore()
+        .collection('sos')
+        .doc(firebase.auth().currentUser.uid)
+        .update({
+          cancelled: true,
+          rescued: true,
+        })
+    } else if (option == 'cancel') {
+      firebase.firestore()
+        .collection('sos')
+        .doc(firebase.auth().currentUser.uid)
+        .update({
+          cancelled: true,
+        })
+    }
+    console.log('SOS alert updated:', option)
+  }
+
+  const sendSosAlert = () => {
+    if (location) {
+      const coords = {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      }
+      const geodata = {
+        geohash: geokit.hash(coords),
+        geopoint: new firebase.firestore.GeoPoint(location.coords.latitude, location.coords.longitude)
+      }
+      const sosData = {
+        g: geodata,
+        uid: firebase.auth().currentUser.uid,
+        username: firebase.auth().currentUser.displayName,
+        cancelled: false,
+        rescued: false,
+      };
+      firebase.firestore()
+        .collection('sos')
+        .doc(firebase.auth().currentUser.uid)
+        .set(sosData, { merge: true })
+        .then((doc) => {
+          console.log('New SOS document added')
+        }).catch((error) => {
+          console.error('Error adding document: ', error)
+        })
+      sosAlert();
+    } else {
+      Alert.alert("No location found");
+    }
+  }
+
+  const getSosAlert = () => {
+    if (location) {
+      const GeoFirestore = geofirestore.initializeApp(firebase.firestore())
+      const geocollection = GeoFirestore.collection('sos')
+      const query = geocollection.near({ center: new firebase.firestore.GeoPoint(location.coords.latitude, location.coords.longitude), radius: 5 })
+      query.where('cancelled', '==', false).onSnapshot(snap => {
+        if (snap.docs.exist) {
+          let userSosData = []
+          let changes = snap.docChanges();
+          for (let change of changes) {
+            userSosData.push(change.doc.data())
+          }
+          setUserSosData(userSosData)
+          receiveSosAlert()
+        } else {
+          console.log("SOS oma uid")
+        }
+      })
+    }
+  }
+
   //UseEffects
   useEffect(() => {
     fetchData();
     getUserLocation();
+    getSosAlert();
     const interval = setInterval(() => {
       fetchData();
       getUserLocation();
-    }, 30000);
+    }, 120000);
     return () => clearInterval(interval);
   }, []);
 
   useEffect(() => {
-    updateUserLocationToFirebase();
+    updateUserLocation();
     getMarkers();
   }, [shipLocations, shipMetadata, userLocations]);
 
   return (
     <Container>
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 60.1587262,
-          longitude: 24.922834,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1,
-        }}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={mapStyleDark}
-        showsMyLocationButton={true}
-      >
-        {shipMarkers.map((res, i) => {
-          const currentTime = Date.now();
-          const vesselIcon =
-            res.shipType > 60
-              ? require("../../assets/cargoshipicon.png")
-              : require("../../assets/boaticon.png");
-          return (
-            <Marker
-              key={i}
-              coordinate={{
-                latitude: res.geometry.coordinates[1],
-                longitude: res.geometry.coordinates[0],
-              }}
-              title={res.mmsi.toString()}
-              description={`${
-                (currentTime - res.properties.timestampExternal) / 1000
-              }s ago, shiptype: ${res.shipType}, ship name: ${res.name}`}
-              image={vesselIcon}
-            />
-          );
-        })}
-        {userMarkers.map((res, i) => {
-          const userIcon =
-            res.uid === fb.auth().currentUser.uid
-              ? require("../../assets/selficon.png")
-              : require("../../assets/usericon.png");
-          return (
-            <Marker
-              key={i}
-              coordinate={{
-                latitude: res.coords.latitude,
-                longitude: res.coords.longitude,
-              }}
-              title={res.uid}
-              description={`username: ${res.username}`}
-              image={userIcon}
-            />
-          );
-        })}
-      </MapView>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 60.1587262,
+            longitude: 24.922834,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={mapStyleDark}
+          showsUserLocation={true}
+        >
+          {shipMarkers.map((res, i) => {
+            const currentTime = Date.now();
+            const vesselIcon =
+              res.shipType > 60
+                ? require("../../assets/cargoshipicon.png")
+                : require("../../assets/boaticon.png");
+            return (
+              <Marker
+                key={i}
+                coordinate={{
+                  latitude: res.geometry.coordinates[1],
+                  longitude: res.geometry.coordinates[0],
+                }}
+                title={res.mmsi.toString()}
+                description={`${(currentTime - res.properties.timestampExternal) / 1000
+                  }s ago, shiptype: ${res.shipType}, ship name: ${res.name}`}
+                image={vesselIcon}
+              />
+            );
+          })}
+          {userMarkers.map((res, i) => {
+            const userIcon =
+              res.uid === firebase.auth().currentUser.uid
+                ? require("../../assets/selficon.png")
+                : require("../../assets/usericon.png");
+            return (
+              <Marker
+                key={i}
+                coordinate={{
+                  latitude: res.coords.latitude,
+                  longitude: res.coords.longitude,
+                }}
+                title={res.uid}
+                description={`username: ${res.username}`}
+                image={userIcon}
+              />
+            );
+          })}
+        </MapView>
+        <View style={{ position: 'absolute', alignSelf: 'flex-end', top: '95%' }}>
+          <Fab
+            active={active}
+            direction="up"
+            containerStyle={{}}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => sendSosAlert()}>
+            <Icon name="medkit" />
+          </Fab>
+        </View>
+      </View>
     </Container>
   );
 };
