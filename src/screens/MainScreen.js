e55173b3e3e6e9fda40f16872040653486e00b8f
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2020-11-19 12:15:33 +0200

    Fab shows

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { Container } from 'native-base';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import { mapStyleDark } from '../styles/MapStyleDark';
+import React, {useEffect, useState} from 'react';
+import {Container, Button, Icon, Fab} from 'native-base';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {mapStyleDark} from '../styles/MapStyleDark';
 
 import fb from '../helpers/Firebase';
 
 const MainScreen = () => {
-  const [shipLocations, setShipLocations] = useState(null);
-  const [shipMetadata, setShipMetadata] = useState(null);
-  const [userLocations, setUserLocations] = useState(null);
-  const [shipMarkers, setShipMarkers] = useState([]);
-  const [userMarkers, setUserMarkers] = useState([]);
+  const [shipLocations,
+    setShipLocations] = useState(null);
+  const [shipMetadata,
+    setShipMetadata] = useState(null);
+  const [userLocations,
+    setUserLocations] = useState(null);
+  const [shipMarkers,
+    setShipMarkers] = useState([]);
+  const [userMarkers,
+    setUserMarkers] = useState([]);
+  const [active,
+    setActive] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = async() => {
     console.log("Fetching data...")
-    const success = res => res.ok ? res.json() : Promise.resolve({});
+    const success = res => res.ok
+      ? res.json()
+      : Promise.resolve({});
     const locations = fetch('https://meri.digitraffic.fi/api/v1/locations/latest').then(success);
     const metadata = fetch('https://meri.digitraffic.fi/api/v1/metadata/vessels').then(success);
 
-    fb.database().ref('/userLocations').orderByKey().once('value').then(snap => {
-      let userLocationsData = [];
-      snap.forEach(childSnap => {
-        userLocationsData.push(childSnap.val())
-      })
-      setUserLocations(userLocationsData);
-    });
+    fb
+      .database()
+      .ref('/userLocations')
+      .orderByKey()
+      .once('value')
+      .then(snap => {
+        let userLocationsData = [];
+        snap.forEach(childSnap => {
+          userLocationsData.push(childSnap.val())
+        })
+        setUserLocations(userLocationsData);
+      });
 
     try {
-      const [locationsFetch, metadataFetch] = await Promise.all([locations, metadata]);
+      const [locationsFetch,
+        metadataFetch] = await Promise.all([locations, metadata]);
       setShipLocations(locationsFetch.features);
       setShipMetadata(metadataFetch);
     } catch (err) {
@@ -40,10 +55,12 @@ const MainScreen = () => {
       console.log("Updating markers...")
       const currentTime = Date.now()
       const filterTime = currentTime - 60000
-      const combinedResult = shipLocations.filter(i => i.properties.timestampExternal >= filterTime).map(locaObj => ({
-        ...shipMetadata.find((metaObj) => (metaObj.mmsi === locaObj.mmsi)),
-        ...locaObj
-      }));
+      const combinedResult = shipLocations
+        .filter(i => i.properties.timestampExternal >= filterTime)
+        .map(locaObj => ({
+          ...shipMetadata.find((metaObj) => (metaObj.mmsi === locaObj.mmsi)),
+          ...locaObj
+        }));
 
       setShipMarkers(combinedResult);
       setUserMarkers(userLocations);
@@ -59,7 +76,10 @@ const MainScreen = () => {
         speed: 30
       }
     }
-    fb.database().ref(`/userLocations/${fb.auth().currentUser.uid}`).set(currentLocationMOCK)
+    fb
+      .database()
+      .ref(`/userLocations/${fb.auth().currentUser.uid}`)
+      .set(currentLocationMOCK)
   }
 
   //UseEffects
@@ -80,43 +100,71 @@ const MainScreen = () => {
   return (
     <Container>
       <MapView
-        style={{ flex: 1 }}
+        style={{
+        flex: 1
+      }}
         initialRegion={{
-          latitude: 60.1587262,
-          longitude: 24.922834,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1
-        }}
+        latitude: 60.1587262,
+        longitude: 24.922834,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1
+      }}
         provider={PROVIDER_GOOGLE}
         customMapStyle={mapStyleDark}
         showsUserLocation={true}
         showsMyLocationButton={true}>
         {shipMarkers.map((res, i) => {
-          return (
-            <Marker
-              key={i}
-              coordinate={{
-                latitude: res.geometry.coordinates[1],
-                longitude: res.geometry.coordinates[0],
-              }}
-              title={res.mmsi.toString()}
-              description={res.shipType.toString()}
-              image={require('../../assets/cargoshipicon.png')} />
-          );
+          return (<Marker
+            key={i}
+            coordinate={{
+            latitude: res.geometry.coordinates[1],
+            longitude: res.geometry.coordinates[0]
+          }}
+            title={res
+            .mmsi
+            .toString()}
+            description={res
+            .shipType
+            .toString()}
+            image={require('../../assets/cargoshipicon.png')}/>);
         })}
         {userMarkers.map((res, i) => {
-          return (
-            <Marker
-              key={i}
-              coordinate={{
-                latitude: res.coords.latitude,
-                longitude: res.coords.longitude,
-              }}
-              title={"user"}
-              image={require('../../assets/usericon.png')} />
-          );
+          return (<Marker
+            key={i}
+            coordinate={{
+            latitude: res.coords.latitude,
+            longitude: res.coords.longitude
+          }}
+            title={"user"}
+            image={require('../../assets/usericon.png')}/>);
         })}
       </MapView>
+      <Fab
+        active={active}
+        direction="up"
+        containerStyle={{}}
+        style={{
+        backgroundColor: '#5067FF'
+      }}
+        position="bottomRight"
+        onPress={() => setActive(!active)}>
+        <Icon name="share"/>
+        <Button style={{
+          backgroundColor: '#34A34F'
+        }}>
+          <Icon name="logo-whatsapp"/>
+        </Button>
+        <Button style={{
+          backgroundColor: '#3B5998'
+        }}>
+          <Icon name="logo-facebook"/>
+        </Button>
+        <Button disabled style={{
+          backgroundColor: '#DD5144'
+        }}>
+          <Icon name="mail"/>
+        </Button>
+      </Fab>
     </Container>
   );
 }
